55e5de8d1ea17d7fb0a4ba206e3eba0aa2c9994f
Gaab <gabriela.oliveira200403@gmail.com>
2024-11-23 17:01:37 -0300

    Add files via upload

---
+++ Aula 5/aula_5.py
@@ -0,0 +1,54 @@
+#1
+m= "Bem-vinda"
+print(m)
+
+#2
+n= bool(5)
+type(n)
+print(n)
+
+#3
+n1=26
+n2=13
+print(n1*n2)
+
+#4
+number1=15
+number2=3
+print(number1/number2)
+
+#5
+numero1=29
+numero2=12
+print(numero1-numero2)
+
+#6
+n3=10
+n4=5
+print(n3/n4)
+
+#7
+n5=3
+n6=5
+n7=7
+n8=9
+print(n5*n6*n7*n8)
+
+#8
+p= '14'
+n9=4
+print(p,n9)
+print(n9+n9)
+
+#9
+n10= int(input('Digite um numero: '))
+n11= int(input('Digite outro numero: '))
+print(n10+n11)
+
+#10
+print('Cadastro')
+nome= input('Nome Completo: ')
+idade= input('Idade: ')
+data= input('Aniversário: ')
+tel= input('Telefone: ')
+
